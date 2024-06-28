a8c1df18512b4ca0602d7f5a2750abcfc6cb076e
Ashwin Gadhvi <ashwingadhvi397@gmail.com>
2024-06-28 23:12:01 +0530

    Adding One Component. Also Learning Some Rules Which help in Practice.

---
+++ Day_1/01vitereact/src/List.jsx
@@ -0,0 +1,15 @@
+function List(){
+    return (
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ul>
+    )
+}
+
+export default List
+
+
+//Always Start Function With capital Latter
+//Always export default
+//return Element
